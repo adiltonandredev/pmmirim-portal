44d15830c3ef8dd10a4a01dadfbeb4eea0bb49a1
adiltonandre <adiltonandre@gmail.com>
2026-02-04 11:48:33 -0400

    Correção AudiLog

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -47,6 +47,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
 }
 
 
+// Validate ../../../src/app/admin/audit/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/admin/audit">> = Specific
+  const handler = {} as typeof import("../../../src/app/admin/audit/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../src/app/admin/banners/[id]/edit/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/admin/banners/[id]/edit">> = Specific
